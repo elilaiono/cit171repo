1fbe766f827281f5fcc26052f00729b40f3ddad6
elilaiono <elilaiono@gmail.com>
2022-11-18 19:55:55 -0700

    camera settings

--- stedi-react-native-private-main/screens/Profile.js
+++ stedi-react-native-private-main/screens/Profile.js
@@ -1,9 +1,11 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Image, SafeAreaView , Share, ScrollView, Button} from 'react-native';
 import { Card, CardTitle, CardContent} from 'react-native-material-cards';
 import BarChart from 'react-native-bar-chart';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Camera } from "expo-camera";
+
 // import Share from 'react-native-share';
 
 
@@ -21,50 +23,138 @@ import { Camera } from "expo-camera";
 // labels
 // const horizontalData = ['S', 'M', 'T', 'W', 'T', 'F','S'];
 
-const Profile = (props) => {
-  const myCustomerShare = async() =>{
-    const shareOptions = {
-      message: 'This is a test'
-    }
-    try{
-      const shareResponse = await Share.share(shareOptions)
-      console.log(shareResponse);
-      }
-      catch(error){
-  console.log('Error', error)
-      }
-    }
-
-  return (
-    <SafeAreaView style={{flex: 1}}>
-         <Card style={{backgroundColor:'white', borderRadius: 10, margin:20 ,width: 320, shadowColor: "#000",
-shadowOffset: {
-	width: 0,
-	height: 2,
-},
-shadowOpacity: 0.23,
-shadowRadius: 2.62,
-
-elevation: 4}}>
-     <CardContent>
-     <Image style={{height: 100, width:100, borderRadius: 75}}
-      source={require('../image/me.jpg')} />
-    <Text style={{marginTop:10,marginBottom:10,fontWeight: 'bold'}}>Sarah Romero</Text>
-
-    <Text style={{marginTop:20,marginBottom:2}}>This Week's progress</Text>
-{/* <BarChart barColor='green' data={data} horizontalData={horizontalData} /> */}
-     <View style={{ marginTop: 50 }}>
-      <Button onPress={myCustomerShare} title="Share" />
-    </View>
-    </CardContent>
-    </Card>
- </SafeAreaView>
-  );
+const camerOptions = {
+  quality: 0,
+  exif: false, 
 };
+
+const Profile = (props) => {
+  const [userName, setUserName] = useState("");
+  const [cameraPermission, setCameraPermission] = useState(false);
+  const [profilePhoto, setProfilePhoto] = useState(null);
+  const [cameraReady, setCameraReady] = useState(false);
+  const cameraRef = useRef(null);
+  useEffect(() => {
+    const getUserName = async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      setCameraPermission(cameraPermission);
+      const userName = await AsyncStorage.getItem("userName");
+      setUserName(userName);
+      await AsyncStorage.removeItem("profilePhoto");
+      setProfilePhoto(profilePhoto);
+    };
+    getUserName();
+  }, []);
+  const myCustomerShare = async () => {
+    const shareOptions = {
+      message: "This is a test",
+    };
+    try {
+      const shareResponse = await Share.share(shareOptions);
+      console.log(shareResponse);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+  if (profilePhoto == null) {
+    return (
+      <View style={styles.container}>
+        <Camera
+          style={styles.camera}
+          ref={cameraRef}
+          onCameraReady={() => {
+            setCameraReady(true);
+          }}
+        >
+          <View style={styles.buttonContainer}>
+            {cameraReady ? (
+              <TouchableOpacity
+                style={styles.button}
+                onPress={async () => {
+                  const picture = await cameraRef.current.takePictureAsync(
+                    cameraOptions
+                  );
+                  console.log("Picture", picture);
+                  await AsyncStorage.setItem("profilePhoto", picture.url);
+                  setProfilePhoto(picture.url);
+                }}
+              >
+                <Text style={styles.Text}>Take Picture</Text>
+              </TouchableOpacity>
+            ) : null}
+          </View>
+        </Camera>
+      </View>
+    );
+  } else {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <Card
+          style={{
+            backgroundColor: "white",
+            borderRadius: 10,
+            margin: 20,
+            width: 320,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.23,
+            shadowRadius: 2.62,
+
+            elevation: 4,
+          }}
+        >
+          <CardContent>
+            <Image
+              style={{ height: 100, width: 100, borderRadius: 75 }}
+              source={{ uri: profilePhoto }}
+            />
+            <Text
+              style={{ marginTop: 10, marginBottom: 10, fontWeight: "bold" }}
+            >
+              {userName}
+            </Text>
+
+            <Text style={{ marginTop: 20, marginBottom: 2 }}>
+              This Week's progress
+            </Text>
+            {/* <BarChart barColor='green' data={data} horizontalData={horizontalData} /> */}
+            <View style={{ marginTop: 50 }}>
+              <Button onPress={myCustomerShare} title="Share" />
+            </View>
+          </CardContent>
+        </Card>
+      </SafeAreaView>
+    );
+  }
+};
+
 export default Profile;
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    padding:20
-  }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
